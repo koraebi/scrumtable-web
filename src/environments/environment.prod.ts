b9c2cb57abc57e14da94d4237118b33cbc88d8cd
Sabri <sabri.habib@etu.unice.fr>
2022-02-12 20:57:57 +0100

    ip update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_ENDPOINT: 'http://52.88.52.11:3000/issues',
-  SOCKET_ENDPOINT: 'http://52.88.52.11:3000'
+  API_ENDPOINT: 'http://54.212.138.51:3000/issues',
+  SOCKET_ENDPOINT: 'http://54.212.138.51:3000'
 };
