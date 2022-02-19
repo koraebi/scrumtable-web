5a1958f4c5d9d54ef88590088ec808a91cd355bc
Nadaelabdellaoui20 <nada.el-abdellaoui@etu.univ-cotedazur.fr>
2022-02-20 00:58:54 +0100

    add detail to issue

--- src/app/core/api/issues.api.ts
+++ src/app/core/api/issues.api.ts
@@ -1,29 +1,55 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { map, Observable } from "rxjs";
-import { Moscow } from "src/app/home/enum/moscow.enum";
-import { Issue } from "../../home/model/issue.model";
-import {environment} from "../../../environments/environment";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Moscow } from 'src/app/home/enum/moscow.enum';
+import { Issue } from '../../home/model/issue.model';
+import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class IssueAPI {
-    readonly API = environment.API_ENDPOINT;
+  readonly API = environment.API_ENDPOINT;
 
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-    getIssues(): Observable<Issue[]> {
-        return this.http.get<Issue[]>(this.API);
-    }
+  getIssues(): Observable<Issue[]> {
+    return this.http.get<Issue[]>(this.API);
+  }
 
-    addLabelToIssue(issue: Issue, label: Moscow): Observable<Issue> {
-        return this.http.post<string[]>(this.API + '/' + issue.number, {label: label}).pipe(map((result) => new Issue(issue.number, issue.name, issue.description, issue.assignee, issue.assignees, issue.selected,
-            result.length === 0 ? undefined : result[0] as Moscow
-            )));
-    }
+  addLabelToIssue(issue: Issue, label: Moscow): Observable<Issue> {
+    return this.http
+      .post<string[]>(this.API + '/' + issue.number, { label: label })
+      .pipe(
+        map(
+          (result) =>
+            new Issue(
+              issue.number,
+              issue.name,
+              issue.description,
+              issue.assignee,
+              issue.assignees,
+              issue.selected,
+              result.length === 0 ? undefined : (result[0] as Moscow)
+            )
+        )
+      );
+  }
 
-    removeLabelToIssue(issue: Issue, label: Moscow): Observable<Issue> {
-        return this.http.delete<string[]>(this.API + '/' + issue.number + '/' + label).pipe(map((result) => new Issue(issue.number, issue.name,issue.description,issue.assignee, issue.assignees, issue.selected,
-            result.length === 0 ? undefined : result[0] as Moscow
-            )));
-    }
+  removeLabelToIssue(issue: Issue, label: Moscow): Observable<Issue> {
+    return this.http
+      .delete<string[]>(this.API + '/' + issue.number + '/' + label)
+      .pipe(
+        map(
+          (result) =>
+            new Issue(
+              issue.number,
+              issue.name,
+              issue.description,
+              issue.assignee,
+              issue.assignees,
+              issue.selected,
+              result.length === 0 ? undefined : (result[0] as Moscow)
+            )
+        )
+      );
+  }
 }
