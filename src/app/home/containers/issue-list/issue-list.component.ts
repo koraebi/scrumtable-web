06eeb994ec23dbad1232b038cfbdefcfc1a46466
Nadaelabdellaoui20 <nada.el-abdellaoui@etu.univ-cotedazur.fr>
2022-02-20 14:59:17 +0100

    fix mistakes of issues

--- src/app/home/containers/issue-list/issue-list.component.ts
+++ src/app/home/containers/issue-list/issue-list.component.ts
@@ -1,38 +1,36 @@
 import { CdkDragDrop } from '@angular/cdk/drag-drop';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Issue } from 'src/app/home/model/issue.model';
-import {IContent} from "../../model/label.model";
+import { IContent } from '../../model/label.model';
 
 @Component({
   selector: 'app-issue-list',
   templateUrl: './issue-list.component.html',
-  styleUrls: ['./issue-list.component.css']
+  styleUrls: ['./issue-list.component.css'],
 })
 export class IssueListComponent implements OnInit {
   @Input()
-  name: string = "";
+  name: string = '';
   p: number = 1;
 
   @Input()
   content?: IContent;
 
   @Output()
-  drop = new EventEmitter<{from: string, to: string, index: number}>();
+  drop = new EventEmitter<{ from: string; to: string; index: number }>();
 
   @Output()
   issueSelected = new EventEmitter<Issue>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onDrop(event: CdkDragDrop<Issue[]>) {
     this.drop.emit({
       from: event.previousContainer.id,
       to: event.container.id,
-      index: event.previousIndex
+      index: event.previousIndex,
     });
   }
-
 }
