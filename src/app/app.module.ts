5098786206223012d5df5d6ca3492b1789bd5bb7
ThomasMartin710 <thomas.martin710@outlook.fr>
2022-02-20 09:46:44 +0100

    remove pagination import inside modules + corrections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,6 @@ import { CoreModule } from './core/core.module';
 import { HomeModule } from './home/home.module';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {NgxPaginationModule} from "ngx-pagination";
-import {SocketIoModule} from "ngx-socket-io";
 import {SocketService} from "./core/services/socket.service";
 
 @NgModule({
@@ -20,9 +18,7 @@ import {SocketService} from "./core/services/socket.service";
     HomeModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    NgxPaginationModule
   ],
-  providers: [SocketService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -8,7 +8,6 @@ import { HomeState } from "./state/home.state";
 
 import {DragDropModule} from '@angular/cdk/drag-drop';
 import { IssueListComponent } from './containers/issue-list/issue-list.component';
-import {NgxPaginationModule} from "ngx-pagination";
 import { DetailsComponent } from './containers/details/details.component';
 import {MatGridListModule} from "@angular/material/grid-list";
 import {MaterialModule} from "../shared/modules/material/material.module";
@@ -16,7 +15,7 @@ import { MiniIssueComponent } from './components/mini-issue/mini-issue.component
 import { DetailsIssueComponent } from './components/details-issue/details-issue.component';
 
 @NgModule({
-  imports: [HomeRoutingModule, SharedModule, DragDropModule, NgxPaginationModule, MatGridListModule, MaterialModule],
+  imports: [HomeRoutingModule, SharedModule, DragDropModule, MatGridListModule, MaterialModule],
     declarations: [
         HomeComponent,
         IssueComponent,
